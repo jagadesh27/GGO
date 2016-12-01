7fcd715599c4293f3ddd79a7631efee8b5cf0ea1
Jack <Jack@Jagannath>
2016-12-02 02:18:50 +0530

    new file added

---
+++ GitSampleTest/src/com/test/Sample/HelloWorld2.java
@@ -0,0 +1,12 @@
+package com.test.Sample;
+
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My first Code");
+		System.out.println("My Second Code");
+	}
+
+}
